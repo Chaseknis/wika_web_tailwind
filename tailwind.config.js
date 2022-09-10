ccd14b143846a59a9b9c17f067b0d02b7bb2c72b
Chase <97366651+Chaseknis@users.noreply.github.com>
2022-09-11 00:11:35 +0200

    aboutUspage&services

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     fontFamily: {
-      'heroTitleFont':['Rubik', '"sans-serif"'],
+      'heroTitleFont':['Rubik', '"sans-serif"'], 
+      'smallHeaderFont':['"Playfair Display"', 'serif'],
       'HeaderFont':['Rosarivo', 'serif'],
       'subHeaderFont':['Montserrat', '"sans-serif"'],
       'paragraphTextFont':['Raleway','"sans-serif"'],
@@ -27,6 +28,20 @@ module.exports = {
       xl:'1440px'
     },
     extend: {
+      gridColumn: {
+        'gridColumn-1': 'span 4 / span 7',
+      },
+      gridRow: {
+        'gridRow-1': 'span 1 / span 3',
+      },
+      gridTemplateColumns: {
+        '16': 'repeat(16, 1fr)',
+        'footer': '200px minmax(900px, 1fr) 100px',
+      },
+      gridTemplateRows: {
+        '6': 'repeat(6, 6rem)',
+        'layout': '200px minmax(900px, 1fr) 100px',
+      },
       dropShadow: {
         'buttonShadow': '0 0.2rem 0.4rem rgba(0, 0, 0, 0.2)',
         'pageTextShadow': '0 0.1rem 0.3rem rgba(0, 0, 0, 0.2)',
@@ -42,7 +57,8 @@ module.exports = {
         White: 'rgb(255, 255, 255)',
         Black: 'rgb(0, 0, 0)',
         Birch: 'rgb(56, 52, 41)',
-        lightBirch: 'rgba(56, 52, 41,0,60)'
+        lightBirch: 'rgba(56, 52, 41,0,60)',
+        dimWhite: 'rgb(245, 245, 245)'
       },
     },
   },
