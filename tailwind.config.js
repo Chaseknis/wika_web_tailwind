250cf90bedffe05b9ab19d322b783e81e6ec6423
Chase <97366651+Chaseknis@users.noreply.github.com>
2022-09-14 08:22:19 +0200

    Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,11 +23,21 @@ module.exports = {
     },
     screens: {
       sm:'480px',
-      md:'768px',
+      md:'768px',            
       lg:'976px',
       xl:'1440px'
     },
     extend: {
+      gridTemplateColumns: {
+        'custom-16': 'repeat(16, 1fr)',
+
+        'footer': '200px minmax(900px, 1fr) 100px',
+      },
+      gridTemplateRows: {
+        'custom-6': 'repeat(6, 6rem)',
+
+        'layout': '200px minmax(900px, 1fr) 100px',
+      },
     dropShadow: {
         'buttonShadow': '0 0.2rem 0.4rem rgba(0, 0, 0, 0.2)',
         'pageTextShadow': '0 0.1rem 0.3rem rgba(0, 0, 0, 0.2)',
@@ -42,8 +52,9 @@ module.exports = {
         dullBrown: 'rgb(134, 113, 73)',
         White: 'rgb(255, 255, 255)',
         Black: 'rgb(0, 0, 0)',
+        lightBlack: 'rgba(0, 0, 0, 0.60)',
         Birch: 'rgb(56, 52, 41)',
-        lightBirch: 'rgba(56, 52, 41,0,60)',
+        lightBirch: 'rgba(56, 52, 41, 0.60)',
         dimWhite: 'rgb(245, 245, 245)'
       },
     },
