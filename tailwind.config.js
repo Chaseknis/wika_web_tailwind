ae912deb31c03483f8f78b3a7b4c311a168432ed
Chase <97366651+Chaseknis@users.noreply.github.com>
2022-10-10 00:53:41 +0200

    industries_page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     fontSize: {
       'tiny': '.75rem',
-      'small': '.875',
+      'small': '.875rem',
       'base': '1rem',
       'lg': '1.5rem',
       'xl': '2rem',
@@ -42,14 +42,29 @@ module.exports = {
         'buttonShadow': '0 0.2rem 0.4rem rgba(0, 0, 0, 0.2)',
         'pageTextShadow': '0 0.1rem 0.3rem rgba(0, 0, 0, 0.2)',
         'heroShadow': '0 0.3rem 0.5rem rgba(0, 0, 0, 0.6)',
-        'cardShadow':'0 0.2rem 1.5rem rgba(0, 0, 0, 0.4)',
+        'cardShadow':'0 0.2rem 1.5rem rgba(0, 0, 0, 0.4)',  
         '4xl': [
             '0 35px 35px rgba(0, 0, 0, 0.25)',
             '0 45px 65px rgba(0, 0, 0, 0.15)'
         ]
       },
+      boxShadow: {
+        'small':'0.5px 1px 1px hsl(220deg 60% 50% / 0.7)',
+        'medium': '0 1px 1px hsl(0deg 0% 0% / 0.075), 0 2px 2px hsl(0deg 0% 0% / 0.075), 0 4px 4px hsl(0deg 0% 0% / 0.075), 0 8px 8px hsl(0deg 0% 0% / 0.075), 0 16px 16px hsl(0deg 0% 0% / 0.075)',
+        'large':'rgba(17, 17, 26, 0.1) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 48px',
+        'test': '1px 2px 2px hsl(220deg 60% 50% / 0.2), 2px 4px 4px hsl(220deg 60% 50% / 0.2), 4px 8px 8px hsl(220deg 60% 50% / 0.2), 8px 16px 16px hsl(220deg 60% 50% / 0.2), 16px 32px 32px hsl(220deg 60% 50% / 0.2)',
+        'testmiddle': 'rgba(240, 46, 170, 0.4) 0px 5px, rgba(240, 46, 170, 0.3) 0px 10px, rgba(240, 46, 170, 0.2) 0px 15px, rgba(240, 46, 170, 0.1) 0px 20px, rgba(240, 46, 170, 0.05) 0px 25px',
+        'xl': ' rgba(0, 0, 0, 0.4) -5px 5px, rgba(0, 0, 0, 0.3) -10px 10px, rgba(0, 0, 0, 0.2) -15px 15px, rgba(0, 0, 0, 0.1) -20px 20px, rgba(0, 0, 0, 0.05) -25px 25px',
+        '1xl': 'rgba(0, 0, 0, 0.2) 0px 18px 50px -10px',
+        '2xl': 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px',
+        '3xl': 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+        '4xl': 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+        '6xl': 'box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;',
+      },
       colors: {
         dullBrown: 'rgb(134, 113, 73)',
+        foggyGrey: 'rgb(211, 204, 184)',
+        barleyWhite: 'rgb(255, 243, 197)',
         White: 'rgb(255, 255, 255)',
         Black: 'rgb(0, 0, 0)',
         lightBlack: 'rgba(0, 0, 0, 0.60)',
