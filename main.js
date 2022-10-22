24354aa43b8daa550b9d62cf5dcdd75e33ce8873
Chase <97366651+Chaseknis@users.noreply.github.com>
2022-10-22 22:29:36 +0200

    links

--- main.js
+++ main.js
@@ -13,6 +13,20 @@ setTimeout (() => {
     database.innerText = "Database of translators";
 }, 4515)
 
+/* function updateList() {
+    const titles = [...document.querySelectorAll('h1, h2')].sort((a, b) => {
+        return Math.abs(a.getBoundingClientRect().top) - Math.abs(b.getBoundingClientRect().top)
+    });
+
+    document.querySelectorAll(".selected-circle").forEach(c => c.classList.remove("selected-circle"));
+    document.querySelectorAll(".nav-dot") [[...document.querySelectorAll('h1, h2')].indexOf(titles[0])].classList.add("selected-circle");
+}
+
+updateList();
+window.addEventListener('scroll', () => {
+    updateList();
+}) */
+
 
 /*function sendEmail() {
     Email.send({
