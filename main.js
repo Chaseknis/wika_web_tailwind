e783f5a6a69621ff012b4692b8b220ab665425e0
chaseknis <chase6th@gmail.com>
2023-01-31 12:55:30 +0200

    make changes

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ form.addEventListener('submit', e => {
 
 
 
-let rootEvent = document.getElementById("rootElement");
+// let rootEvent = document.getElementById("rootElement");
 
 // let burgerNav = document.getElementById("burger-time");
 
@@ -91,40 +91,40 @@ let rootEvent = document.getElementById("rootElement");
 
 
 
-(function ($) {
+// (function ($) {
 
-    "use strict";
+//     "use strict";
   
-      // PRE LOADER
-      $(window).load(function(){
-        $('.preloader').fadeOut(1000); // set duration in brackets    
-      });
+//       // PRE LOADER
+//       $(window).load(function(){
+//         $('.preloader').fadeOut(1000); // set duration in brackets    
+//       });
   
   
-      // MENU
-      $('#navbar-navbar-collapse a').on('click',function(){
-        $("#navbar-navbar-collapse").collapse('hide');
-      });
+//       // MENU
+//       $('#navbar-navbar-collapse a').on('click',function(){
+//         $("#navbar-navbar-collapse").collapse('hide');
+//       });
   
-      $(window).scroll(function() {
-        if ($("#navbar-navbar").offset().top > 50) {
-          $("#navbar-navbar-fixed-top").addClass("top-nav-collapse");
-            } else {
-              $("#navbar-navbar-fixed-top").removeClass("top-nav-collapse");
-            }
-      });
+//       $(window).scroll(function() {
+//         if ($("#navbar-navbar").offset().top > 50) {
+//           $("#navbar-navbar-fixed-top").addClass("top-nav-collapse");
+//             } else {
+//               $("#navbar-navbar-fixed-top").removeClass("top-nav-collapse");
+//             }
+//       });
   
-      // SMOOTHSCROLL
-      $(function() {
-        $('#custom-navbar a, #home a').on('click', function(event) {
-          var $anchor = $(this);
-            $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top - 49
-            }, 1000);
-              event.preventDefault();
-        });
-      });  
+//       // SMOOTHSCROLL
+//       $(function() {
+//         $('#custom-navbar a, #home a').on('click', function(event) {
+//           var $anchor = $(this);
+//             $('html, body').stop().animate({
+//               scrollTop: $($anchor.attr('href')).offset().top - 49
+//             }, 1000);
+//               event.preventDefault();
+//         });
+//       });  
   
-  })(jQuery);
+//   })(jQuery);
 
   
